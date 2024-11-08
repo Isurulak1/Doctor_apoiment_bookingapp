0ad2aab209cc51c501f5b59b210c77d5c2268b9b
Isurulak1 <isurulakshan0417@gmail.com>
2024-11-09 00:58:40 +0530

    Finish project setup

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Home from '../pages/Home'
+import Services from '../pages/Services'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Contact from '../pages/Contact'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorDetails from '../pages/Doctors/DoctorDetails'
+
+
+import {Routes, Route} from 'react-router-dom'
 
 const Routers = () => {
   return (
-    <div>Routers</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path='/home' element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/login" element={<Login />} />
+      <Route path='/doctor' element={<Doctors />} />
+      <Route path='/doctor/:id' element={<DoctorDetails />} />
+      <Route path="/register" element={<Signup />} />
+    </Routes>
   )
 }
 
