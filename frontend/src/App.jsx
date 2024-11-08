ef277d789d2aa5423764e4c1c6592aade4feeb11
Isurulak1 <isurulakshan0417@gmail.com>
2024-11-08 17:58:57 +0530

    Setup page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Layout from './layout/Layout'
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <Layout />
     </>
   )
 }
