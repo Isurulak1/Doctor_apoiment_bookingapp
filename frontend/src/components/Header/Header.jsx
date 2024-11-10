25f2b1359f5eff5070c86177c97512fd12acdd4d
Isurulak1 <isurulakshan0417@gmail.com>
2024-11-10 15:46:10 +0530

    header navigations link

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, {useEffect,useRef} from 'react'
+import logo from '../../assets/images/logo.png'
+import {NavLink, Link} from 'react-router-dom';
+
+const navLinks = [
+  {
+    path: '/home',
+    display: 'Home'
+  },
+  {
+    path: '/doctors',
+    display: 'Find a Doctor'
+  },
+  {
+    path: '/services',
+    display: 'Services'
+  },
+  {
+    path: '/contact',
+    display: 'Contact'
+  },
+]
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header >
+      <div>
+        <div>
+
+          {/* ============ logo ============ */}
+          <div>
+            <img src={logo} alt="" />
+          </div>
+
+          {/* ================= menu ================  */}
+          <div>
+            <ul>
+              {navLinks.map((link,index)=>(
+                <li key={index}>
+                  <NavLink
+                   to={link.path}
+                   className={navClass =>
+                    navClass.isActive
+                    ? "text-primaryColor text-[16px] leading-7 font-[600]"
+                    : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
+                   }
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
