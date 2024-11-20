6ea22e30a6d9cad5cbcfd8b47232b70ba9a30d97
Isurulak1 <isurulakshan0417@gmail.com>
2024-11-20 20:21:10 +0530

    created about content

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import aboutImg from '../../assets/images/about.png'
 import aboutCarding from '../../assets/images/about-card.png'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 const About = () => {
   return (
@@ -15,7 +16,22 @@ const About = () => {
                     </div>
                 </div>
 
-                ====================== about content ===========================
+                {/* ====================== about content =========================== */}
+                <div className='w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2'>
+                    <h2 className='heading'>Proud to be one of the nations best</h2>
+                        <p className='text_para'>
+                         Content is the foundation of digital communication, shaping how businesses and individuals interact with their audiences. From informative blog posts to engaging social media updates, content drives engagement, builds trust, and communicates value. High-quality content not only informs and entertains but also fosters deeper connections by addressing the needs and interests of its target audience.
+                        </p>
+
+                        <p className='text_para mt-[30px]'>
+                         Content is the foundation of digital communication, shaping how businesses and individuals interact with their audiences. From informative blog posts to engaging social media updates, content drives engagement, builds trust, and communicates value. High-quality content not only informs and entertains but also fosters deeper connections by addressing the needs and interests of its target audience.
+                        </p>
+
+                        <Link to='/'>
+                            <button className='btn'>Learn More</button>
+                        </Link>
+                    
+                </div>
             </div>
         </div>
     </section>
