ec6d32e25f6c0d5cbe9dc50a02356f950aebb762
Isurulak1 <isurulakshan0417@gmail.com>
2024-11-29 18:21:10 +0530

    fix error
    fixing error

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -1,45 +1,47 @@
-import React from 'react'
-import { Pagination } from 'swiper'; 
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/pagination'
-import patientAvatar from '../../assets/images/patient-avatar.png'
-import {HiStar} from 'react-icons/hi'
-
-SwiperCore.use([Pagination]);
+import React from 'react';
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import patientAvatar from '../../assets/images/patient-avatar.png';
+import { HiStar } from 'react-icons/hi';
 
 const Testimonial = () => {
   return (
     <div>
-        <Swiper modules={[Pagination]} spaceBetween={30} slidesPerView={1} pagination={{clickable:true}}
+      <Swiper
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
         breakpoints={{
-            640: {
-              slidesPerView: 1,
-              spaceBetween: 0,
-            },
-            768: {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            1024: {
-              slidesPerView: 3,
-              spaceBetween: 30,
-            },
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
         }}
-        >
-            <SwiperSlide>
-              <div className='py-[30px] px-5 rounded-3'>
-                <div className='flex items-center gap-[13px]'>
-                    <img src={patientAvatar} alt="" />
-                </div>
-                <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>
-                    Muhibar Raman
-                </h4>
-              </div>  
-            </SwiperSlide>
-        </Swiper>
+      >
+        <SwiperSlide>
+          <div className="py-[30px] px-5 rounded-3">
+            <div className="flex items-center gap-[13px]">
+              <img src={patientAvatar} alt="Patient Avatar" />
+            </div>
+            <h4 className="text-[18px] leading-[30px] font-semibold text-headingColor">
+              Muhibar Raman
+            </h4>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonial
+export default Testimonial;
