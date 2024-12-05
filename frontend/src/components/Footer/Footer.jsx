dc863ece5e0e6cf47c6175447bfe9013339b766d
Isurulak1 <isurulakshan0417@gmail.com>
2024-12-05 22:58:30 +0530

    style footer jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,19 +8,19 @@ import {AiFillYoutube,AiFillGithub,AiOutlineInstagram} from 'react-icons/ai'
 const socialLinks = [
   {
     path: "https://www.youtube.com/",
-    Icon : <AiFillYoutube />,
+    Icon : <AiFillYoutube className='group-hover:text-white w-4 h-5' />,
   },
   {
     path: "https://www.github.com/",
-    Icon : <AiFillGithub />,
+    Icon : <AiFillGithub className='group-hover:text-white w-4 h-5'/>,
   },
   {
     path: "https://www.instergram.com/",
-    Icon : <AiOutlineInstagram />,
+    Icon : <AiOutlineInstagram className='group-hover:text-white w-4 h-5'/>,
   },
   {
     path: "https://www.linkedln.com/",
-    Icon : <RiLinkedinFill />,
+    Icon : <RiLinkedinFill className='group-hover:text-white w-4 h-5'/>,
   }
 ]
 
@@ -66,15 +66,39 @@ const quickLinks03 = [
   {
     path: "/",
     display: "Donate",
-  }
+  },
   {
     path: "/contact",
     display: "Contact Us",
   }
 ]
 const Footer = () => {
+
+  const year = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
+    <footer className='pb-16 pt-10'>
+      <div className='container'>
+        <div className='flex justify-between flex-col md:flex-row flex-wrap gap-[30px]'>
+          <div>
+            <img src={logo} alt="" />
+            <p className='text-[16px] leading-2 font-[400] text-textColor'>Copyright @ {year} developed by isuru all right reversed.</p>
+
+            <div className='flex items-center gap-3 mt-4'>
+              {socialLinks.map((link,index) => 
+                 <Link
+                  to={link.path}
+                  key={index}
+                  className='w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none'
+                 >
+                 {link.Icon}
+                 </Link>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
