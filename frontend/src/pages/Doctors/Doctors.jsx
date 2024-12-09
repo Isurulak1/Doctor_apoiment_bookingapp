57a32a31de0f620fd26edd0b51fce67ddc92e959
Isurulak1 <isurulakshan0417@gmail.com>
2024-12-09 18:21:33 +0530

    create a search bar in doctor page

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Doctors = () => {
   return (
-    <div>Doctors</div>
+    <>
+      <section className='bg-[#fff9ce]'>
+        <div className='container text-center'>
+          <h2 className='heading'>Find a Doctpor</h2>
+            <div className='max-w-[570px] mt-[30px] mx-auto bg-[#0066f2] rounded-md flex item-center justify-between'>
+              <input type="search" className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor placeholder:Search' />
+            </div>
+          
+        </div>
+      </section>
+    </>
   )
 }
 
