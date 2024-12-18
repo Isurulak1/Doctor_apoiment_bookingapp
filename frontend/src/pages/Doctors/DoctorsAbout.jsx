15b472372efc7cda742644fe8542b72d56e3e438
Isurulak1 <isurulakshan0417@gmail.com>
2024-12-18 17:30:00 +0530

    Update DoctorsAbout.jsx

--- frontend/src/pages/Doctors/DoctorsAbout.jsx
+++ frontend/src/pages/Doctors/DoctorsAbout.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const DoctorsAbout = () => {
   return (
-    <div>DoctorsAbout</div>
+    <div>
+      <div>
+        <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>About of
+          <span className='text-irisBlueColor font-bold text-[24px] leading-9'>
+            Muhibur Rahman
+          </span>
+        </h3>
+        <p className='text_para'>
+        The term "doctor" is used to refer to a person who has a doctorate, usually a PhD, or to a medical practitioner. In the past, the term was used to describe any learned person.
+        </p>
+      </div>
+    </div>
   )
 }
 
