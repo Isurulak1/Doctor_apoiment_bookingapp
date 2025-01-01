e2245bb7b242fc859e1b00e993bd6dc2f0ae4af7
Isurulak1 <isurulakshan0417@gmail.com>
2025-01-01 08:30:57 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
 
@@ -26,8 +27,29 @@ const Login = () => {
             name='email'
             value={FormData.email}
             onChange={handleInputChange}
-             />
+            className='w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer'
+            required
+            />
           </div>
+          <div className='mb-5'>
+            <input type="password"
+            placeholder='Password'
+            name='password'
+            value={FormData.email}
+            onChange={handleInputChange}
+            className='w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer'
+            required
+            />
+          </div>
+          <div className='mt-7'>
+            <button className='w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3'>
+              Login
+            </button>
+          </div>
+          <p className='mt-5 text-textColor text-center'>
+            Don&apos;t have an account? 
+            <Link to='/register' className='text-primaryColor font-medium ml-1'>Register</Link>
+          </p>
         </form>
       </div>
     </section>
